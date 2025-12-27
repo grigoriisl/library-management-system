d3c338ea992098f2e8f05b364e026dbbbb4ee0af
grigoriisl <slyusarenko.grigory@mail.ru>
2025-12-27 12:39:53 +0300

    Add Main class skeleton

---
+++ src/main/java/library/Main.java
@@ -0,0 +1,8 @@
+package library;
+
+public class Main {
+    public static void main(String[] args) {
+        Library library = new Library();
+        // Демонстрационный код будет добавлен в следующем коммите
+    }
+}
